fddf6343a6c810148144a3b59846f9a26db6e8a4
Nicolas <nicolascarriere30@gmail.com>
2023-06-12 14:25:11 -0400

    Configuration des routes et dependencies.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Helmet>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fjalla+One&display=swap" />
+      </Helmet>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </>
   );
 }
 
