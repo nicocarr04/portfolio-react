a0ec520ebd718306c5696fd0c10aadf0cfe3bdee
Nicolas <nicolascarriere30@gmail.com>
2023-06-12 14:30:53 -0400

    Configuration des routes, dependencies.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Header from './components/Header';
 
-function App() {
+function App() { 
   return (
     <>
       <Helmet>
